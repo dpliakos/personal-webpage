af1431c4d2518b9e54a2419657312b90a6b65153
dpliakos <dimitrispl96@gmail.com>
2019-06-16 02:43:58 +0300

    chore: add github and gitlab links.

--- src/components/pages/contact/contact.tsx
+++ src/components/pages/contact/contact.tsx
@@ -9,6 +9,8 @@ const OpenSourcePage = (props: any) => {
       <ul>
         <li> dimitrispl96@gmail.com </li>
         <li> dpliakos@it.teithe.gr </li>
+        <li> <a href="https://github.com/dPliakos/personal-webpage"> github  </a></li>
+        <li> <a href="https://gitlab.com/dPliakos">gitlab </a> </li>
         <li> <a href="https://www.facebook.com/dimitris.pliakos.52"> Facebook </a> </li>
         <li> <a href="https://twitter.com/Dimitris_Pl"> twitter </a> </li>
       </ul>
