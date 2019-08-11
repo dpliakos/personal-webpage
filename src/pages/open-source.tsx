b0717408c9b64fe01be061a79783ec43d8046e98
dpliakos <dimitrispl96@gmail.com>
2019-08-12 00:30:19 +0300

    fix: add the social media at the open source page too.

--- src/pages/open-source.tsx
+++ src/pages/open-source.tsx
@@ -6,9 +6,11 @@ import Page from "./../components/pages/open-source/open-source";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const PageWrapper = (props: any) => {
   const person = props.data.allPersonalInfoJson.nodes[0];
+  const socialMedia = props.data.allSocialMediaJson.nodes[0].social_media;
 
   const headerData = {
-    person: person,
+    person,
+    socialMedia
   };
 
   return <Page headerData={headerData} />;
