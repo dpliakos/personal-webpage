bc1a1cd63a1ba2fd5ce6c080e4b2d79b5f8f4a5d
Dimitris Pliakos <dimitrispl96@gmail.com>
2019-10-31 20:49:16 +0200

    feat(social-media): Add the email list.

---
+++ src/components/components/social-media-list/social-media-list.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import './social-media-list.scss';
+
+
+class SocialMediaList extends React.PureComponent {
+  
+  constructor(props: any) {
+    super(props);
+    
+    this.state = {
+      expanded: false
+    }
+  }
+  
+  render() {
+    const snapshot = {...this.state};
+    const extraClass = snapshot.expanded ? 'social-media-list--expanded' : '';
+
+    const list = this.props.items.map((item, key) => {
+      return (
+        <ul className="social-media-list__list" key={key}>
+          <li className="social-media-list__list-item"> 
+            <a href={item.link}> {item.label} </a> 
+          </li>
+        </ul>
+      );
+    });
+
+    return (
+      <div className={`social-media-list ${extraClass}`}>
+        <div className="social-media-list__list-wrapper">
+          {list}
+        </div>
+        <button onClick={() => {this.setState({expanded: !this.state.expanded})}} className="social-media-list__button">
+          <this.props.icon />
+        </button>
+      </div>  
+    ); 
+  }
+}
+
+export default SocialMediaList;
